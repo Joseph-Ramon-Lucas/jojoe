c283c4370684a635b927ebd19589b98bb6160f2f
Joseph-Ramon-Lucas <joseph-ramon-lucas@gmail.com>
2024-12-30 02:13:12 -0500

    fixed up some sql schema and added dummy data
    Co-authored-by: Johanna Arguello <JohannaArguelloG@gmail.com>

--- setup.sql
+++ setup.sql
@@ -5,22 +5,45 @@ CREATE DATABASE jojoereviews;
 
 \c jojoereviews
 
-
-CREATE TABLE Movies (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(255),
-    rating INTEGER,
-    thoughts TEXT,
-    author VARCHAR(255)
-);
--- hello johanna
-CREATE TABLE Authors (
+CREATE TABLE authors (
     id INTEGER PRIMARY KEY,
     author_name VARCHAR(255)
 );
 
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    rating INTEGER,
+    thoughts TEXT,
+    author INTEGER,
 
-INSERT INTO Authors(author_name) VALUES ("Johanna");
--- INSERT INTO Authors(id, author_name) VALUES (2, "Joe");
+    Foreign Key (author) REFERENCES authors(id)
+);
+-- hello johanna
+
+
+
+INSERT INTO authors (id, author_name) VALUES (1, 'Johanna');
+INSERT INTO authors(id, author_name) VALUES (2, 'Joe');
+
+INSERT INTO movies(id, title, rating, thoughts, author) 
+VALUES (1, 'Don’t Worry Darling', 9, 'Suspenseful with a mystery. The twist ending mid way through exposing another Simulated world turned the movie around and made it more interesting.', 2),  
+(2, 'Sex in the City', 9, 'Very fun, fashionable, and ridiculously entertaining for some reason.', 1 );
+
+
+select * from authors;
+select * from movies;
+
+select * from movies join authors on movies.author = authors.id;
+
+-- joining movies authors with the author IDs, so that movies with "Johanna" as author is returned.
+select title author_name from movies join authors on movies.author = authors.id;
+
+-- where conditional helps specify your results
+select title, author from movies where author = 1;
+
+-- combining join + where
+select title, author, author_name from movies join authors on movies.author = authors.id where author_name = 'Johanna';
+
 
 
